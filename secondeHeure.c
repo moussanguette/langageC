abc957164d5be491dedc1d7a4724fecc90db43e9
Moussa Nguette <mossanguette@gmail.com>
2020-11-09 10:46:23 +0000

    conversion de secondes en heures, minutes et secondes

---
+++ secondeHeure.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int h,m,s;
+	printf("donner le nombre de seconde !");
+	scanf("%d",&s);
+	h=s/3600;
+	m=(s%3600)*60/3600;
+	s=(((s%3600)*60)%3600)*60/3600;
+	printf("%dH : %dM : %dS\n",h,m,s);
+}
+	
